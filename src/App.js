b23f1c9f2d49f7098b9790a0a863da3d065c758a
David Espinoza <despinoza1027@gmail.com>
2018-11-13 15:47:34 -0600

    show KeepItClassy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-// import KeepItClassy from './KeepItClassy/KeepItClassy'
+import KeepItClassy from './KeepItClassy/KeepItClassy'
 import HooksCounter from './HooksCounter/HooksCounter'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <KeepItClassy /> */}
-        <HooksCounter />
+        <KeepItClassy />
+        {/* <HooksCounter /> */}
       </div>
     );
   }
